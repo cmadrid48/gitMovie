fdda7174e297e7688785c18e282bb05f33837583
Tina Wang <tinaw@Tinas-MBP.lan>
2023-01-30 21:35:00 -0800

    added sample movie search form and sample single-item carousel

---
+++ assets/Js/main.js
@@ -0,0 +1,15 @@
+// carousel functionalities
+const slidesContainer = document.getElementById("slides-container");
+const slide = document.querySelector(".slide");
+const prevButton = document.getElementById("slide-arrow-prev");
+const nextButton = document.getElementById("slide-arrow-next");
+
+nextButton.addEventListener("click", () => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft += slideWidth;
+});
+
+prevButton.addEventListener("click", () => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft -= slideWidth;
+});
